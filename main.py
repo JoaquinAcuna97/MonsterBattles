ee54e2b5a728b8214701074b968771b1846a1e8c
joaco <joaco@LAPTOP-G9SRR02E>
2024-04-05 11:52:40 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+git clone https://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@github.com/fullstacklabs/assessment-cc-python-django-rest-sr-01.git --branch v2.0.0
